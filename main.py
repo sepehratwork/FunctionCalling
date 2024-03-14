a53b930c045b2ee57f93e25d0bc9ab7cca6255e4
Sepehr Kerachi <85013316+sepehratwork@users.noreply.github.com>
2024-03-14 19:25:27 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates/")
 
-@app.get("/")
-def home():
-    return {"message": "Hey There!"}
+
+@app.get('/')
+def read_form():
+    return 'hello world'
+
+
+@app.get("/get_prompt")
+def get_prompt(request: Request):
+    result = "Type a number"
+    return templates.TemplateResponse('prompt.html', context={'request': request, 'result': result})
+
+
+# @app.post("/prompt_response")
+# def prompt_response(request: Request, num: int = Form(...)):
+#     result = spell_number(num)
+#     return templates.TemplateResponse('prompt.html', context={'request': request, 'result': result})
